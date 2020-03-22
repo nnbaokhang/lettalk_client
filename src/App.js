719c0f70f5ae1b7e4af8cac436d76a8232c4d3a6
nnbaokhang <nnbaokhang@gmail.com>
2020-03-21 18:27:48 -0600

    Add basic signup and room

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { useHistory, Route, Router, BrowserRouter, Redirect} from 'react-router-dom';
 import logo from './logo.svg';
+import Box from '@material-ui/core/Box';
+import { Button } from 'reactstrap';
+import { browserHistory } from 'react-router';
 import './App.css';
+class App extends Component {
+    state = {
+        redirect: false
+    }
+    setRedirect = () => {
+        this.setState({
+            redirect: true
+        })
+    }
+    renderRedirect = () => {
+        if (this.state.redirect) {
+          //this.props.history.push("/room");
+            return <Redirect to='/room' />
+        }
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <form>
+                        <label>
+                            Name:
+                            <input type="text" name="name"/>
+                        </label>
+                        <Route>
+                        {this.renderRedirect()}
+                        </Route>
+                        <button type="button" onClick={this.setRedirect.bind()}>
+                        Submit
+                    </button>
+                    </form>
+                </div>
+            </BrowserRouter>
+        )
+    }
 }
+
 
 export default App;
